aecaaeb263663243a88b0ff2fee95162a372eeda
caozongan <caozongan@qq.com>
2021-01-13 11:46:44 +0800

    2021.01.13_HBLW_MAIN初始模板 包含ssh的pom、hiberante.xml等..

---
+++ src/test/java/com/hbwl/test/Hello.java
@@ -0,0 +1,7 @@
+package com.hbwl.test;
+
+/**
+ * Created by cza 2021-01-13 10:56
+ */
+public class Hello {
+}
---
+++ src/test/java/com/hbwl/test/db/Db_Ora_Test.java
@@ -0,0 +1,10 @@
+package com.hbwl.test.db;
+
+/**
+ * Created by cza 2021-01-13 10:58
+ */
+public class Db_Ora_Test {
+    public static void main(String[] args) {
+        System.out.println("--");
+    }
+}

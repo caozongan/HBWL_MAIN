6e3aef697aa5a0a8ab1f01833c29866ad863c66d
caozongan <caozongan@qq.com>
2021-01-13 16:22:53 +0800

    2021.01.13_HBLW_MAIN初始模板001

---
+++ Study/src/main/java/com/hbwl/study/StudyTest.java
@@ -0,0 +1,16 @@
+package com.hbwl.study;
+
+import com.hbwl.bean.Hello;
+
+/**
+ * Created by cza 2021-01-13 16:00
+ */
+public class StudyTest {
+    public static void main(String[] args) {
+        System.out.println("学习！！！");
+        Hello hello = new Hello();
+        //hello.sayHello("cza");
+        System.out.println(hello.sayHello("cza"));
+
+    }
+}
--- src/main/java/com/hbwl/bean/Hello.java
+++ src/main/java/com/hbwl/bean/Hello.java
@@ -4,9 +4,6 @@ package com.hbwl.bean;
  * Created by cza 2021-01-13 9:37
  */
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("--");
-    }
 
     public String sayHello(String name){
         return "Hello"+name+"!";

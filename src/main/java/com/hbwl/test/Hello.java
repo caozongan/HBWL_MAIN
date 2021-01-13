ad0c1ac8c616ec8417030d31f2b16f44cf3b1552
caozongan <caozongan@qq.com>
2021-01-13 10:09:32 +0800

    2021.01.13

---
+++ src/main/java/com/hbwl/test/Hello.java
@@ -0,0 +1,10 @@
+package com.hbwl.test;
+
+/**
+ * Created by cza 2021-01-13 9:37
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("--");
+    }
+}
---
+++ src/main/java/com/hbwl/test/HelloGit.java
@@ -0,0 +1,10 @@
+package com.hbwl.test;
+
+/**
+ * Created by cza 2021-01-13 9:56
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("++");
+    }
+}

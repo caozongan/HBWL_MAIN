a0fcc1ded1a1cfae5d27e81412cd9a3e36fb8959
caozongan <caozongan@qq.com>
2021-01-13 23:48:50 +0800

    2021.01.13_HBLW_MAIN初始模板006 修改pom文件，替换version变量

---
+++ Web_Ssh/src/main/java/com/hbwl/test/HelloSsh.java
@@ -0,0 +1,10 @@
+package com.hbwl.test;
+
+/**
+ * Created by cza 2021-01-13 23:47
+ */
+public class HelloSsh {
+    public static void main(String[] args) {
+        System.out.println("----!!");
+    }
+}
